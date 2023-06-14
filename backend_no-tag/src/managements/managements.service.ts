20824cc41c541995c2c55b665a6543d8134eff4c
ChunHee Han <chunhee.han@outlook.com>
2023-06-14 17:14:47 +0900

    feat: constructor에 레파지토리들 주입 into service

--- backend_no-tag/src/managements/managements.service.ts
+++ backend_no-tag/src/managements/managements.service.ts
@@ -2,8 +2,23 @@ import { Injectable } from '@nestjs/common';
 import { CreateManagementDto } from './dto/create-management.dto';
 import { UpdateManagementDto } from './dto/update-management.dto';
 
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Brand } from './entities/brand.entity';
+import { Category } from './entities/category.entity';
+import { Product } from './entities/product.entity';
+
 @Injectable()
 export class ManagementsService {
+  constructor(
+    @InjectRepository(Brand)
+    private brandRepository: Repository<Brand>,
+    @InjectRepository(Category)
+    private categoryRepository: Repository<Category>,
+    @InjectRepository(Product)
+    private productRepository: Repository<Product>,
+  ) {}
+
   create(createManagementDto: CreateManagementDto) {
     return 'This action adds a new management';
   }
