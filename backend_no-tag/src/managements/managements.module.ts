59a63f6528872f47c164f2d0607cf794cd47a889
ChunHee Han <chunhee.han@outlook.com>
2023-06-14 17:07:42 +0900

    feat: 임포트 엔티티들 into 모듈

--- backend_no-tag/src/managements/managements.module.ts
+++ backend_no-tag/src/managements/managements.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { ManagementsService } from './managements.service';
 import { ManagementsController } from './managements.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Brand } from './entities/brand.entity';
+import { Category } from './entities/category.entity';
+import { Product } from './entities/product.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Brand, Category, Product])],
   controllers: [ManagementsController],
-  providers: [ManagementsService]
+  providers: [ManagementsService],
 })
 export class ManagementsModule {}
