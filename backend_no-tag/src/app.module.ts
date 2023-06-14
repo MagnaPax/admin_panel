9729b94c5b998b51ac242df20744f0420c772698
ChunHee Han <chunhee.han@outlook.com>
2023-06-14 16:51:21 +0900

    feat: TypeORM 세팅

--- backend_no-tag/src/app.module.ts
+++ backend_no-tag/src/app.module.ts
@@ -2,9 +2,14 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ManagementsModule } from './managements/managements.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ormConfig } from './orm.config';
 
 @Module({
-  imports: [ManagementsModule],
+  imports: [
+    ManagementsModule,
+    TypeOrmModule.forRootAsync({ useFactory: ormConfig }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
