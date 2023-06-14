0f8432fdcd565558cdbd243dc0dc6ca08f022e36
ChunHee Han <chunhee.han@outlook.com>
2023-06-14 16:58:37 +0900

    feat: DTO 수정

--- backend_no-tag/src/managements/dto/create-management.dto.ts
+++ backend_no-tag/src/managements/dto/create-management.dto.ts
@@ -1,1 +1,21 @@
 export class CreateManagementDto {}
+
+export class BrandDto {
+  id: number;
+  name: string;
+}
+
+export class CategoryDto {
+  id: number;
+  name: string;
+}
+
+export class ProductDto {
+  id: number;
+  name: string;
+  brand: BrandDto;
+  category: CategoryDto;
+  gender: string;
+  isKids: boolean;
+  salesQuantity: number;
+}
