2fd037031b1e9a90116b2257b8c7c40004e5c591
ChunHee Han <chunhee.han@outlook.com>
2023-06-15 15:01:24 +0900

    feat: category 작업
    db연결, 라우팅, REST API 비지니스 로직

--- backend_no-tag/src/category/category.service.ts
+++ backend_no-tag/src/category/category.service.ts
@@ -1,26 +1,43 @@
 import { Injectable } from '@nestjs/common';
 import { CreateCategoryDto } from './dto/create-category.dto';
 import { UpdateCategoryDto } from './dto/update-category.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Category } from './entities/category.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class CategoryService {
-  create(createCategoryDto: CreateCategoryDto) {
-    return 'This action adds a new category';
+  constructor(
+    @InjectRepository(Category)
+    private categoryRepository: Repository<Category>,
+  ) {}
+
+  async create(createCategoryDto: CreateCategoryDto) {
+    return await this.categoryRepository.save(createCategoryDto);
   }
 
-  findAll() {
-    return `This action returns all category`;
+  async findAll() {
+    return await this.categoryRepository.find();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} category`;
+  async findOne(category_id: number) {
+    return await this.categoryRepository.findOne({ where: { category_id } });
   }
 
-  update(id: number, updateCategoryDto: UpdateCategoryDto) {
-    return `This action updates a #${id} category`;
+  async update(category_id: number, updateCategoryDto: UpdateCategoryDto) {
+    const category = await this.findOne(category_id);
+    if (!category) {
+      throw new Error('Not found the category');
+    }
+    Object.assign(category, updateCategoryDto);
+    return await this.categoryRepository.save(category);
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} category`;
+  async remove(category_id: number) {
+    const category = await this.findOne(category_id);
+    if (!category) {
+      throw new Error('Not found the category');
+    }
+    return await this.categoryRepository.remove(category);
   }
 }
