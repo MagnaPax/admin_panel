5d68f982d08a0d140c8ae58c645ff4eb218df95d
ChunHee Han <chunhee.han@outlook.com>
2023-06-15 16:38:27 +0900

    feat: 엔티티 수정 of product

--- backend_no-tag/src/product/entities/product.entity.ts
+++ backend_no-tag/src/product/entities/product.entity.ts
@@ -1,1 +1,35 @@
-export class Product {}
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  ManyToOne,
+  JoinColumn,
+} from 'typeorm';
+import { Brand } from '../../brand/entities/brand.entity';
+import { Category } from '../../category/entities/category.entity';
+
+@Entity()
+export class Product {
+  @PrimaryGeneratedColumn()
+  product_id: number;
+
+  @Column()
+  product_name: string;
+
+  @ManyToOne(() => Brand)
+  @JoinColumn({ name: 'brand_id' })
+  brand: Brand;
+
+  @ManyToOne(() => Category)
+  @JoinColumn({ name: 'category_id' })
+  category: Category;
+
+  @Column()
+  sex: string;
+
+  @Column()
+  is_kids: boolean;
+
+  @Column()
+  sales_quantity: number;
+}
