fd98ea6b90120cc27473e417a3c36341ee257fd7
ChunHee Han <chunhee.han@outlook.com>
2023-06-24 22:02:20 +0900

    feat: 엔티티 주입 into 카테고리 모듈

--- backend_no-tag/src/category/category.module.ts
+++ backend_no-tag/src/category/category.module.ts
@@ -3,9 +3,11 @@ import { CategoryService } from './category.service';
 import { CategoryController } from './category.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Category } from './entities/category.entity';
+import { Product } from 'src/product/entities/product.entity';
+import { Brand } from 'src/brand/entities/brand.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Category])],
+  imports: [TypeOrmModule.forFeature([Category, Product, Brand])],
   controllers: [CategoryController],
   providers: [CategoryService],
 })
