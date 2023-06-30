6be258564bf7035c16156e77496335cb73fe38d4
ChunHee Han <chunhee.han@outlook.com>
2023-06-30 21:07:58 +0900

    feat: 데이터 추가 수정 of Category
    - 입력값이 중복되면 거부
    - 중간테이블에 brand_id, category_id 추가
    - category_id 가 중복되는 중간 테이블 레코드들 정리

--- backend_no-tag/src/category/dto/create-category.dto.ts
+++ backend_no-tag/src/category/dto/create-category.dto.ts
@@ -1,8 +1,18 @@
-import { IsDefined, IsString, IsNotEmpty } from 'class-validator';
+import {
+  IsDefined,
+  IsString,
+  IsNotEmpty,
+  IsNumber,
+  IsOptional,
+} from 'class-validator';
 
 export class CreateCategoryDto {
   @IsNotEmpty()
   @IsDefined()
   @IsString()
   category_name: string;
+
+  @IsOptional()
+  @IsNumber()
+  brand_id: number;
 }
