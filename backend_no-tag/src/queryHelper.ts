28380368f5fe9ac3db286c337f39e137c5b177f6
ChunHee Han <chunhee.han@outlook.com>
2023-07-03 22:28:17 +0900

    feat: Query 검색 수정

--- backend_no-tag/src/queryHelper.ts
+++ backend_no-tag/src/queryHelper.ts
@@ -3,18 +3,6 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class Query {
-  async findRecordsById(
-    id: number,
-    tableName: string,
-    columnName: string,
-    repository: Repository<any>,
-  ): Promise<any[]> {
-    return repository
-      .createQueryBuilder(tableName)
-      .where(`${tableName}.${columnName} = :id`, { id })
-      .getMany();
-  }
-
   async findRecordsByValues(
     values: any[],
     columnNames: string[],
@@ -23,15 +11,34 @@ export class Query {
     const queryBuilder = repository.createQueryBuilder();
     const tableName = repository.metadata.name;
 
+    const conditions = [];
+
     columnNames.forEach((columnName, index) => {
       if (values[index] === null) {
-        queryBuilder.andWhere(`${tableName}.${columnName} IS NULL`);
+        conditions.push(`${tableName}.${columnName} IS NULL`);
       } else {
-        queryBuilder.andWhere(`${tableName}.${columnName} = :value${index}`, {
-          [`value${index}`]: values[index],
-        });
+        conditions.push(`${tableName}.${columnName} = :value${index}`);
+        queryBuilder.setParameter(`value${index}`, values[index]);
       }
     });
+
+    queryBuilder.where(conditions.join(' OR '));
+
     return await queryBuilder.getMany();
+  }
+
+  async updateRecords(
+    records: any[],
+    updateProps: Record<string, any>,
+    repository: Repository<any>,
+  ) {
+    for (const record of records) {
+      for (const key in updateProps) {
+        if (updateProps.hasOwnProperty(key)) {
+          record[key] = updateProps[key];
+        }
+      }
+      await repository.save(record);
+    }
   }
 }
