b72e4ba473b083d68fe8043aa43ddd72e001d7a2
ChunHee Han <chunhee.han@outlook.com>
2023-07-06 00:08:51 +0900

    feat: 유효성 검사 for Product 검색
    - 쿼리스트링을 원하는 자료형으로 변환

--- backend_no-tag/src/main.ts
+++ backend_no-tag/src/main.ts
@@ -4,7 +4,11 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true, // 클라이언트에서 보낸 쿼리'스트링'을 컨트롤러에서 알맞는 자료형으로 바꿀 수 있게
+    }),
+  );
   await app.listen(5425);
 }
 bootstrap();
--- backend_no-tag/src/product/product.controller.ts
+++ backend_no-tag/src/product/product.controller.ts
@@ -6,11 +6,14 @@ import {
   Patch,
   Param,
   Delete,
+  Query,
 } from '@nestjs/common';
+
 import { ProductService } from './product.service';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { RemoveProductDto } from './dto/remove-product.dto';
+import { SearchProductDto } from './dto/search-product.dto';
 
 @Controller('product')
 export class ProductController {
@@ -24,6 +27,12 @@ export class ProductController {
   @Get()
   findAll() {
     return this.productService.findAll();
+  }
+
+  @Get('search')
+  search(@Query() query: SearchProductDto) {
+    const { pName, sex, kids, qty } = query;
+    return this.productService.lookUp(pName, sex, kids, qty);
   }
 
   @Get(':id')
