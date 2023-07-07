75d4552ec5be7fbe5c398d86156240853cb8e708
ChunHee Han <chunhee.han@outlook.com>
2023-07-07 22:05:12 +0900

    feat: 파일 업로드
    - 파일 이름이 중복되지 않게
    - 파일 이름이 영어가 아닐 경우 처리
    - 파일 크기 1MB 이하
    - 파일 갯수 최대 3개까지

--- backend_no-tag/src/files/files.controller.ts
+++ backend_no-tag/src/files/files.controller.ts
@@ -1,34 +1,56 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  UseInterceptors,
+  UploadedFile,
+  UploadedFiles,
+} from '@nestjs/common';
+import { FileInterceptor, FilesInterceptor } from '@nestjs/platform-express';
+import { MulterOptions } from '@nestjs/platform-express/multer/interfaces/multer-options.interface';
+import { Multer, diskStorage } from 'multer';
+
 import { FilesService } from './files.service';
-import { CreateFileDto } from './dto/create-file.dto';
-import { UpdateFileDto } from './dto/update-file.dto';
+import { extname } from 'path';
+
+const options: MulterOptions = {
+  storage: diskStorage({
+    destination: './storage',
+    filename: (req, file, callback) => {
+      const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
+      let filename = file.originalname;
+
+      if (!/^[a-zA-Z0-9_.()-]+$/.test(filename)) {
+        // 파일 이름이 영어가 아닌 경우
+        const ext = extname(file.originalname);
+        filename = `file-${uniqueSuffix}${ext}`;
+      } else {
+        const ext = extname(file.originalname);
+        filename = `${file.originalname}-${uniqueSuffix}${ext}`;
+      }
+
+      callback(null, filename);
+    },
+  }),
+  limits: {
+    // 파일 사이즈 1MB
+    fileSize: 1048576,
+  },
+};
 
 @Controller('files')
 export class FilesController {
   constructor(private readonly filesService: FilesService) {}
 
-  @Post()
-  create(@Body() createFileDto: CreateFileDto) {
-    return this.filesService.create(createFileDto);
+  @Post('upload')
+  @UseInterceptors(FileInterceptor('file', options))
+  async uploadFile(@UploadedFile() file: Express.Multer.File) {
+    console.log(file);
+    return `file upload API`;
   }
 
-  @Get()
-  findAll() {
-    return this.filesService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.filesService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateFileDto: UpdateFileDto) {
-    return this.filesService.update(+id, updateFileDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.filesService.remove(+id);
+  @Post('uploads')
+  @UseInterceptors(FilesInterceptor('files', 3, options))
+  uploadFiles(@UploadedFiles() files: Express.Multer.File[]) {
+    console.log(files);
   }
 }
