b26ddf25556bfc700f6c04bdf0cce45a08304579
ChunHee Han <chunhee.han@outlook.com>
2023-07-15 21:10:48 +0900

    feat: 목록 조회 후 store 저장

--- client/src/api/common.ts
+++ client/src/api/common.ts
@@ -1,4 +1,4 @@
-import axios from 'axios'
+import axios, { type AxiosResponse } from 'axios'
 import { setInterceptors } from './interceptors'
 import { useCounterStore } from '@/stores/counter'
 
@@ -20,10 +20,25 @@ export default class CommonApi {
     return `${this.host}${path}`
   }
 
+  async saveResult(path: string, response: AxiosResponse<any, any>) {
+    switch (path) {
+      case 'brand':
+        this.counterStore.setBrandList(response.data)
+        break
+      case 'category':
+        this.counterStore.setCategoryList(response.data)
+        break
+      case 'product':
+        this.counterStore.setCategoryList(response.data)
+        break
+      default:
+        break
+    }
+  }
+
   async get(path: string) {
     const requestURL = this.settingURL(path)
     const response = await this.axiosInstance.get(requestURL)
-
-    this.counterStore.setBrandList(response.data)
+    this.saveResult(path, response)
   }
 }
