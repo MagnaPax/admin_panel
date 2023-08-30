1ba508abbcbe1f58fbcf42b0756b0b6439edc250
ChunHee Han <chunhee.han@outlook.com>
2023-08-30 20:16:50 +0900

    feat: 숫자와 boolean 검색 추가 of Product

--- server/src/queryHelper.ts
+++ server/src/queryHelper.ts
@@ -1,5 +1,6 @@
 import { Repository } from 'typeorm';
 import { Injectable } from '@nestjs/common';
+import { type } from 'os';
 
 @Injectable()
 export class Query {
@@ -14,11 +15,13 @@ export class Query {
     const conditions = [];
 
     columnNames.forEach((columnName, index) => {
-      if (values[index] === null) {
+      const value = values[index];
+
+      if (value === null) {
         conditions.push(`${tableName}.${columnName} IS NULL`);
-      } else {
+      } else if (typeof value === 'string') {
         // 단어를 하나 이상 포함하는지 검사하는 조건을 추가
-        const keywords = values[index].split(/\s+/).filter(Boolean);
+        const keywords = value.split(/\s+/).filter(Boolean);
         if (keywords.length > 0) {
           const keywordConditions = keywords.map(
             (keyword) => `${tableName}.${columnName} LIKE :keyword${index}`,
@@ -28,6 +31,14 @@ export class Query {
             queryBuilder.setParameter(`keyword${index}`, `%${keyword}%`);
           });
         }
+      } else if (typeof value === 'number') {
+        // 숫자 검색
+        conditions.push(`${tableName}.${columnName} = :value${index}`);
+        queryBuilder.setParameter(`value${index}`, value);
+      } else if (typeof value === 'boolean') {
+        // boolean 검색
+        conditions.push(`${tableName}.${columnName} = :value${index}`);
+        queryBuilder.setParameter(`value${index}`, value);
       }
     });
 
